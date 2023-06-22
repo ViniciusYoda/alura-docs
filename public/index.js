7eba6614d2be9cab88a55f812a8690b489be8de5
Vinícius Yoda <vyoda4604@gmail.com>
2023-06-22 17:11:27 -0300

    node - alterando

--- public/index.js
+++ public/index.js
@@ -1,8 +1,20 @@
 import { emitirAdicionarDocumento } from "./socket-front-index.js";
+import { obterCookie, removerCookie } from "./utils/cookies.js";
+
+const tokenJwt = obterCookie("tokenJwt");
+
+console.log(tokenJwt);
 
 const listaDocumentos = document.getElementById("lista-documentos");
 const form = document.getElementById("form-adiciona-documento");
 const inputDocumento = document.getElementById("input-documento");
+const botaoLogout = document.getElementById("botao-logout");
+
+botaoLogout.addEventListener("click", () => {
+  removerCookie("tokenJwt");
+  alert("Usuário deslogado com sucesso!");
+  window.location.href = "/login/index.html";
+});
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
@@ -13,7 +25,7 @@ form.addEventListener("submit", (evento) => {
 function inserirLinkDocumento(nomeDocumento) {
   listaDocumentos.innerHTML += `
     <a
-      href="documento.html?nome=${nomeDocumento}"
+      href="/documento/index.html?nome=${nomeDocumento}"
       class="list-group-item list-group-item-action"
       id="documento-${nomeDocumento}"
     >
